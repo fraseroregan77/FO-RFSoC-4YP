2f1eaac749eb9e4a8c73735eade8be8853e56850
Fraser O'Regan (Student) <fraser.o-regan.2020@uni.strath.ac.uk>
2025-02-26 10:34:50 +0000

    ahhhhhhhhh

---
+++ 2x2_files/omg_please_work/omg_please_work.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,59 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+--Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+--Date        : Tue Feb 25 18:46:47 2025
+--Host        : EEE-R448-03 running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+  port (
+    adc0_clk_clk_n : in STD_LOGIC;
+    adc0_clk_clk_p : in STD_LOGIC;
+    dac0_clk_clk_n : in STD_LOGIC;
+    dac0_clk_clk_p : in STD_LOGIC;
+    sysref_in_diff_n : in STD_LOGIC;
+    sysref_in_diff_p : in STD_LOGIC;
+    vin0_01_v_n : in STD_LOGIC;
+    vin0_01_v_p : in STD_LOGIC;
+    vout00_v_n : out STD_LOGIC;
+    vout00_v_p : out STD_LOGIC
+  );
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  port (
+    sysref_in_diff_n : in STD_LOGIC;
+    sysref_in_diff_p : in STD_LOGIC;
+    adc0_clk_clk_n : in STD_LOGIC;
+    adc0_clk_clk_p : in STD_LOGIC;
+    vin0_01_v_n : in STD_LOGIC;
+    vin0_01_v_p : in STD_LOGIC;
+    dac0_clk_clk_n : in STD_LOGIC;
+    dac0_clk_clk_p : in STD_LOGIC;
+    vout00_v_n : out STD_LOGIC;
+    vout00_v_p : out STD_LOGIC
+  );
+  end component design_1;
+begin
+design_1_i: component design_1
+     port map (
+      adc0_clk_clk_n => adc0_clk_clk_n,
+      adc0_clk_clk_p => adc0_clk_clk_p,
+      dac0_clk_clk_n => dac0_clk_clk_n,
+      dac0_clk_clk_p => dac0_clk_clk_p,
+      sysref_in_diff_n => sysref_in_diff_n,
+      sysref_in_diff_p => sysref_in_diff_p,
+      vin0_01_v_n => vin0_01_v_n,
+      vin0_01_v_p => vin0_01_v_p,
+      vout00_v_n => vout00_v_n,
+      vout00_v_p => vout00_v_p
+    );
+end STRUCTURE;
