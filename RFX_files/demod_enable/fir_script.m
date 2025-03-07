14e039a9f876086d7067e9711f9e0bd3bc3a8648
Fraser O'Regan <Fraseroregan77@Gmail.com>
2025-03-07 15:14:35 +0000

    dskjhdskjh

--- RFX_files/demod_enable/fir_script.m
+++ RFX_files/demod_enable/fir_script.m
@@ -1,9 +1,11 @@
 N = 70;
 Fc = 270e3;
+fs = 2.048e9;
 f = [0 Fc/(fs/2) 1];
 m = [1 1 0];
 coeff = fir2(N, f, m);
 
+freqz(coeff)
 figure;
 plot(demod.Time, demod.Data);
 xlabel("\textbf{Time (s)}", 'Interpreter','latex');
