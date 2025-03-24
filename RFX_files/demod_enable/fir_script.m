2e4d6e23c600d71966cd12cb9f7ccf35c2c39685
Fraser O'Regan (Student) <fraser.o-regan.2020@uni.strath.ac.uk>
2025-03-24 20:17:50 +0000

    stress

--- RFX_files/demod_enable/fir_script.m
+++ RFX_files/demod_enable/fir_script.m
@@ -11,3 +11,29 @@ plot(demod.Time, demod.Data);
 xlabel("\textbf{Time (s)}", 'Interpreter','latex');
 ylabel('\textbf{Amplitude}', 'Interpreter','latex');
 title('\textbf{Demodulated signal ($f_c=500MHz$)}', 'Interpreter','latex');
+%%
+
+tdata = tdata_out.Data;
+ttime = tdata_out.Time;
+
+castData = precast.Data;
+castTime = precast.Time;
+
+Ts = mean(diff(ttime)); % Sampling period
+Fs = 1/Ts; % Sampling frequency
+
+% Compute FFT
+N = length(tdata); % Number of samples
+Y = fft(tdata); % Compute FFT
+f = (0:N-1)*(Fs/N); % Frequency vector
+
+% Compute magnitude spectrum (normalized)
+Y_mag = abs(tdata)/N;
+
+% Plot the FFT results
+figure;
+plot(f(1:N/2), 2*Y_mag(1:N/2)); % Plot only first half (positive frequencies)
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('FFT of Simulink Output');
+grid on;
