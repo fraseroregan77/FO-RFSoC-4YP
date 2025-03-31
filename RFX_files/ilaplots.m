6ba342b8ead8d91efe18c67ba532de7390b6944b
Fraser O'Regan <Fraseroregan77@Gmail.com>
2025-03-31 23:13:26 +0100

    report backup and some changes

--- RFX_files/ilaplots.m
+++ RFX_files/ilaplots.m
@@ -1,52 +1,87 @@
 data = csvread("iladata.csv", 2);
 
 figure;
+subplot(2, 1, 1)
 plot(data(:, 1), data(:, 6), LineWidth=1.5);
 xlabel("\textbf{Number of Samples}", 'Interpreter','latex')
 ylabel("\textbf{Magnitude}", 'Interpreter','latex')
 title("\textbf{ILA Signal (m\_axis\_tdata)}", 'Interpreter','latex')
-xlim([0 750]);
-
-fs = 333e6;
-X = fft(data(:, 6));
-Xabs = abs(X); % Obtain the magnitude
-N = length(Xabs);
-fgrid = fs*(0:(N-1))/(N);
-Xabs = Xabs(1:floor(N/4));
-fgrid = fgrid(1:floor(N/4));
-
-figure;
-plot(fgrid,Xabs);
-xlabel("\textbf{Frequency (Hz)}", 'Interpreter','latex')
+xlim([0 1600])
+grid on
+subplot(2, 1, 2)
+plot(data(:, 1), data(:, 6), LineWidth=1.5);
+xlabel("\textbf{Number of Samples}", 'Interpreter','latex')
 ylabel("\textbf{Magnitude}", 'Interpreter','latex')
-title("\textbf{FFT of ILA Signal (m\_axis\_tdata)}", 'Interpreter','latex')
-ylim([0 1e7])
-xlim([0 7e7])
+title("\textbf{ILA Signal (m\_axis\_tdata)}", 'Interpreter','latex')
+grid on
+xlim([0 300]);
 
+% Sampling frequency
+fs = 333e6;
+
+% Compute FFT
+X = fft(data(:, 6)); 
+N = length(X); % Number of samples
+
+% Normalize magnitude
+Xabs = abs(X) / N;  
+
+% Shift FFT to center zero frequency
+X_shifted = fftshift(Xabs);
+
+% Generate two-sided frequency axis
+fgrid = (-N/2:N/2-1) * (fs/N); 
+
+% Plot two-sided spectrum
+figure;
+plot(fgrid, X_shifted, 'LineWidth', 1.5);
+xlabel("\textbf{Frequency (Hz)}", 'Interpreter', 'latex');
+ylabel("\textbf{Magnitude}", 'Interpreter', 'latex');
+title("\textbf{Two-Sided FFT of ILA Signal (m\_axis\_tdata)}", 'Interpreter', 'latex');
+ylim([0 max(X_shifted)*1.1]); % Adjust y-axis limit
+xlim([-fs/2 fs/2]); % Show full two-sided range
+grid on;
 %%
 data = csvread("ILA_Data_saxisTX.csv", 2);
 
 figure;
+subplot(2, 1, 1)
+plot(data(:, 1), data(:, 4), LineWidth=1.5);
+xlabel("\textbf{Number of Samples}", 'Interpreter','latex')
+ylabel("\textbf{Magnitude}", 'Interpreter','latex')
+title("\textbf{ILA Signal (m\_axis\_tx\_tdata)}", 'Interpreter','latex')
+xlim([0 16000])
+grid on
+subplot(2, 1, 2)
 plot(data(:, 1), data(:, 4), LineWidth=1.5);
 xlabel("\textbf{Number of Samples}", 'Interpreter','latex')
 ylabel("\textbf{Magnitude}", 'Interpreter','latex')
 title("\textbf{ILA Signal (m\_axis\_tx\_tdata)}", 'Interpreter','latex')
 xlim([300 1100]);
-%xlim([0 16000])
 grid on
 
-fs = 300e6;
-X = fft(data(:, 4));
-Xabs = abs(X); % Obtain the magnitude
-N = length(Xabs);
-fgrid = fs*(0:(N-1))/(N);
-Xabs = Xabs(1:floor(N/4));
-fgrid = fgrid(1:floor(N/4));
+% Sampling frequency
+fs = 333e6; 
 
+% Compute FFT
+X = fft(data(:, 4)); 
+N = length(X); % Number of samples
+
+% Normalize magnitude
+Xabs = abs(X) / N;  
+
+% Shift FFT to center zero frequency
+X_shifted = fftshift(Xabs);
+
+% Generate two-sided frequency axis
+fgrid = (-N/2:N/2-1) * (fs/N); 
+
+% Plot two-sided spectrum
 figure;
-plot(fgrid,Xabs);
-xlabel("\textbf{Frequency (Hz)}", 'Interpreter','latex')
-ylabel("\textbf{Magnitude}", 'Interpreter','latex')
-title("\textbf{FFT of ILA Signal (m\_axis\_tx\_tdata)}", 'Interpreter','latex')
-ylim([0 1e7])
-xlim([0 7e7])
+plot(fgrid, X_shifted, 'LineWidth', 1.5);
+xlabel("\textbf{Frequency (Hz)}", 'Interpreter', 'latex');
+ylabel("\textbf{Magnitude}", 'Interpreter', 'latex');
+title("\textbf{Two-Sided FFT of ILA Signal (m\_axis\_tx\_tdata)}", 'Interpreter', 'latex');
+ylim([0 max(X_shifted)*1.1]); % Adjust y-axis limit
+xlim([-fs/2 fs/2]); % Show full two-sided range
+grid on;
